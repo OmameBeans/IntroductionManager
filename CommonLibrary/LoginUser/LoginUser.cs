4b112c2a3b067eb028dd9c2ecb92acc33e0f078a
OmameBeans <hinoddaichi1218@gmail.com>
2024-05-27 00:32:43 +0900

    add CommonLibrary

---
+++ CommonLibrary/LoginUser/LoginUser.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonLibrary.LoginUser
+{
+    public static class LoginUser
+    {
+        public static string GetUserName()
+        {
+            return "5636";
+        }
+    }
+}
---
+++ CommonLibrary/Result/Result.cs
@@ -0,0 +1,29 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CommonLibrary.Results
+{
+    public class Result<T>(T data)
+    {
+        public bool HasError { get; set; } = false;
+        public IList<string> ErrorMessages { get; set; } = [];
+        public T Data { get; set; } = data;
+
+        public void AddMessage(string message)
+        {
+            ErrorMessages.Add(message);
+            HasError = true;
+        }
+    }
+
+    public class Result
+    {
+        public bool HasError { get; set; } = false;
+        public IList<string> ErrorMessages { get; set; } = [];
+
+        public void AddMessage(string message)
+        {
+            ErrorMessages.Add(message);
+            HasError = true;
+        }
+    }
+}
